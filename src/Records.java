fee01649bc6bf9ef5069398cbe323c1bf3b3fb17
OLENUA <71031846+OLENUA@users.noreply.github.com>
2023-04-10 11:56:35 +0300

    Add files via upload

--- src/Records.java
+++ src/Records.java
@@ -1,36 +1,87 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
-class Records{
-    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
-    private String suename;
-    private String phone;
-    private String birthDay;
+    class Record {
+    private String surname;
+    private String phoneNumber;
+    private String birthDate;
     private String comment;
+    private Record[] records;
 
-    public Records(String suename, String phone, String birthDay, String comment) {
-        this.suename = suename;
-        this.phone = phone;
-        this.birthDay = birthDay;
+        public Record(String surname, String phoneNumber, String birthDate) {
+            this.surname = surname;
+            this.phoneNumber = phoneNumber;
+            this.birthDate = birthDate;
+            this.records = new Record[0];
+        }
+
+
+        public Record(String fileName) throws IOException {
+        ArrayList<Record> records = new ArrayList<Record>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader("res\\as.txt"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] data = line.split(",");
+                String surname = data[0];
+                String phoneNumber = data[1];
+                String birthDate = data[2];
+                Record record = new Record(surname, phoneNumber, birthDate);
+                records.add(record);
+            }
+        }
+
+        this.records = records.toArray(new Record[records.size()]);
+    }
+
+
+    public Record[] getRecords() {
+        return records;
+    }
+
+    public void setRecords(Record[] records) {
+        this.records = records;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
         this.comment = comment;
     }
-    public void birthDay(){
-        System.out.println(birthDay);
+
+    public String retrieveBirthDate() {
+        return "Birth date: " + this.birthDate;
     }
-    public void setComment() throws IOException{
-        System.out.println("set comment : ");
-        String coment = reader.readLine();
-        System.out.println("seted coment: " + coment);
-    }
-    public void noComent(){
-        System.out.println("No comments! ;)");
-    }
-    public void read() throws IOException {
-        File file = new File("C:\\Users\\ОлексійЛуцкін\\Documents\\laba2\\res\\as.txt");
-        BufferedReader br = new BufferedReader(new FileReader(file));
-        String st;
-        while ((st = br.readLine()) != null)
-            System.out.println(st);
+
+    public void addComment(String newComment) {
+        this.comment = newComment;
     }
 }
